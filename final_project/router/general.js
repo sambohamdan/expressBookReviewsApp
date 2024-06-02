47cf6204e1dc416e576c6089a90b58a2f29b2c27
Sam Bo Hamdan <virusam91@gmail.com>
2024-06-02 04:44:49 -0400

    Integrate async/await logic for the general calls

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -55,25 +55,25 @@ public_users.get('/isbn/:isbn', async function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
-    getBooks()
+    await getBooks()
     .then((bookEntries) => Object.values(bookEntries))
-    .then((books) => books.filter((book) => book.author === author))
+    .then((books) => books.filter((book) => book.author.toLowerCase().includes(author.toLowerCase())))
     .then((filteredBooks) => res.send(filteredBooks));
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     const title = req.params.title;
-    getBooks()
+    await getBooks()
     .then((bookEntries) => Object.values(bookEntries))
-    .then((books) => books.filter((book) => book.title === title))
+    .then((books) => books.filter((book) => book.title.toLowerCase().includes(title.toLowerCase())))
     .then((filteredBooks) => res.send(filteredBooks));
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
+public_users.get('/review/:isbn', function (req, res) {
     const isbn = req.params.isbn;
 
     if (!isDBEmpty) {
